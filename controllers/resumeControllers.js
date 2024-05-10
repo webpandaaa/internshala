5d1db9cecf7198162e63bc51b38e5b53172cd85a
webpandaaa <ankushrajsingh2004@gmail.com>
2024-05-10 22:33:05 +0530

    education part added

--- controllers/resumeControllers.js
+++ controllers/resumeControllers.js
@@ -1,10 +1,42 @@
 const { catchAsyncErrors } = require("../middlewares/catchAsync");
 const Student = require("../models/studentModel");
 const ErrorHandler = require("../utils/ErrorHandler");
+const { v4: uuidv4 }  = require('uuid');
 
 
 
 exports.resume = catchAsyncErrors(async (req,res,next) =>{
     const { resume } = await Student.findById(req.id).exec();
     res.json({message : "Secure Resume Page!" , resume});
+})
+
+exports.addeducation = catchAsyncErrors(async (req,res,next) =>{
+    const student = await Student.findById(req.id).exec();
+    student.resume.education.push({...req.body , id:uuidv4()});
+    await student.save();
+    res.json({message : "Education Added!"});
+})
+
+
+exports.editeducation = catchAsyncErrors(async (req,res,next) => {
+    const student = await Student.findById(req.id).exec();
+    const eduIndex = student.resume.education.findIndex(
+        (i) => i.id === req.params.eduid
+    );
+    student.resume.education[eduIndex] = {
+        ...student.resume.education[eduIndex],
+        ...req.body, 
+    };
+    await student.save();
+    res.json({ message : "Education updated!"})
+})
+
+exports.deleteeducation = catchAsyncErrors(async (req,res,next) => {
+    const student = await Student.findById(req.id).exec();
+    const filterededu = student.resume.education.filter(
+        (i) => i.id !== req.params.eduid
+    );
+    student.resume.education =  filterededu;
+    await student.save();
+    res.json({ message : "Education deleted!"})
 })
--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router  = express.Router();
 const { isAuthenticated } = require("../middlewares/auth");
-const { resume } = require("../controllers/resumeControllers");
+const { resume ,
+    addeducation,
+    editeducation, 
+    deleteeducation,
+} = require("../controllers/resumeControllers");
 
 
 //Get/
 router.get("/" , isAuthenticated ,resume);
+
+
+// Post / add-education
+router.post("/add-edu" , isAuthenticated , addeducation)
+
+// Post / edit education
+router.post("/edit-edu/:eduid" , isAuthenticated , editeducation)
+
+// Post / delete education
+router.post("/delete-edu/:eduid" , isAuthenticated , deleteeducation)
 
 module.exports = router;
